06faeb1dd2430b59143d31efaa2bf3c51c7c5f2e
Elijah2Hill <90879350+Elijah2Hill@users.noreply.github.com>
2022-03-20 18:13:46 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,29 +1,16 @@
 const button1 = document.getElementById('button1');
 
-button1.addEventListener('click', function(event) {
-  console.log(event);
-  const h3 = document.createElement('h3');
-  h3.textContent = "Foo";
-  document.body.appendChild(h3);
-  
-})
+button1.addEventListener('click', function() { showButtonText('<h3>Foo</h3>') });
 
 const button2 = document.getElementById('button2');
 
-button2.addEventListener('click', function(event) {
-  console.log(event);
-  const h3 = document.createElement('h3');
-  h3.textContent = "Bar";
-  document.body.appendChild(h3);
-  
-})
+button2.addEventListener('click', function() { showButtonText('<h3>Bar</h3>') });
 
 const button3 = document.getElementById('button3');
 
-button3.addEventListener('click', function(event) {
-  console.log(event);
-  const h2 = document.createElement('h2');
-  h2.textContent = "Foobar";
-  document.body.appendChild(h2);
-  
-})
+button3.addEventListener('click', function() { showButtonText('<h2>Foobar</h2>') });
+
+function showButtonText(HTML) {
+  const main = document.getElementById('main');
+  main.innerHTML += HTML;
+}
